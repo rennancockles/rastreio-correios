589af36c913f3eb16ca497e9d02652f53f4485dc
Rennan Cockles <rennancockles@gmail.com>
2021-09-05 23:42:47 -0300

    check script

---
+++ scripts/check.sh
@@ -0,0 +1,10 @@
+#!/bin/sh -e
+
+export SOURCE_FILES="correios"
+
+set -x
+
+black --check --diff --target-version=py36 $SOURCE_FILES
+flake8 $SOURCE_FILES
+mypy --show-error-codes $SOURCE_FILES
+isort --check --diff --project=correios $SOURCE_FILES
