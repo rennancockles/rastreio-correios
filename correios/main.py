c31d7193e74528dfc46c65e32da8914f5a8c1781
Rennan Cockles <rennancockles@gmail.com>
2021-09-06 00:42:40 -0300

    add funcionalidades to correios

--- correios/main.py
+++ correios/main.py
@@ -44,21 +44,18 @@ class Correios:
 
         return None
 
-    def geraCodValido(self, cod: str):
+    def gera_codigo_valido(self, cod: str):
         cod = cod.strip()
 
-        if len(cod) < 12 or 13 < len(cod):
+        if 13 < len(cod):
             return ""
 
-        prefixo = cod[0:2]
-        numero = cod[2:10]
+        prefixo = cod[:2]
         sufixo = cod[-2:]
+        numero = cod[2:-2][:8]
         multiplicadores = [8, 6, 4, 2, 3, 5, 9, 7]
 
-        if len(numero) < 8 and len(cod) == 12:
-            diferenca = 8 - len(numero)
-            zeros = "0" * diferenca
-            numero = zeros + numero
+        numero = self.format_num(numero)
 
         soma = sum(int(numero[i]) * multiplicadores[i] for i in range(8))
         resto = soma % 11
@@ -73,3 +70,35 @@ class Correios:
         codfinal = prefixo + numero + dv + sufixo
 
         return codfinal
+
+    def match_cep(self, cep: str, cod: str) -> bool:
+        if r := self.rastreio(cod):
+            return r.cepDestino == cep
+        return False
+
+    def format_num(self, num: str) -> str:
+        if len(num) < 8:
+            diferenca = 8 - len(num)
+            zeros = "0" * diferenca
+            return zeros + num
+        return num
+
+    def busca_por_cep(
+        self, cep: str, start_cod: str, previous: int = 0, next: int = 10
+    ):
+        cep = cep.replace("-", "")
+        prefixo = start_cod[0:2]
+        numero = int(start_cod[2:10])
+        sufixo = start_cod[-2:]
+
+        for p in range(previous):
+            test_cod = self.gera_codigo_valido(f"{prefixo}{numero - (p + 1)}{sufixo}")
+            if self.match_cep(cep, test_cod):
+                return test_cod
+
+        for n in range(next):
+            test_cod = self.gera_codigo_valido(f"{prefixo}{numero + n}{sufixo}")
+            if self.match_cep(cep, test_cod):
+                return test_cod
+
+        return ""
