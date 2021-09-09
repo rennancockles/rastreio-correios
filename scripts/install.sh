50ff13b1fc9f3ec92ca0b1b27f1edd384fd07e1c
Rennan Cockles <rennancockles@gmail.com>
2021-09-09 10:10:45 -0300

    update requirements.txt

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,9 +1,5 @@
 #!/bin/sh -e
 
-REQUIREMENTS="requirements.txt"
-
-[ $1 == "-d" -o $1 == "-D" ] && REQUIREMENTS="dev-requirements.txt"
-
 set -x
 
-pip install -r "$REQUIREMENTS"
+pip install -r requirements.txt
